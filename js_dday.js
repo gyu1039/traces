272d7da8381c330bc204d74e666daec8b84ab2b6
gyu1039 <gyu1039@naver.com>
2020-09-24 13:54:09 +0900

    Update js_dday.js

--- js_dday.js
+++ js_dday.js
@@ -16,7 +16,7 @@ function calcDate(days) {
     var future = toFirst + days*(1000*60*60*24);
     var someday = new Date(future);
     var year = someday.getFullYear();
-    var month = someday.getMonth();
+    var month = someday.getMonth()+1;
     var date = someday.getDate();
     document.querySelector("#date"+days).innerText = year + "년 " + month + "월 " + date + "일";
 }
